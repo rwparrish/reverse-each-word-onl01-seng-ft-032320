ea10f96d87d4a5d4781f5526571b29489730ff2a
Ryan Parrish <rino3717@gmail.com>
2020-03-26 01:11:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(sentence)
- sentence.split(",")
- rev_sen = []
- sentence.each { |sentence| rev_sen << word.reverse}
+  sentence.split(",")
+  rev_sen = []
+  sentence.each { |sentence| rev_sen << sentence.reverse}
+  end
+  rev_sen.join(" ")
 end
