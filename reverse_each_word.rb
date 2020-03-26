2f8a1e3ff93a25c9fc71dc885e7ec0c2fe2e7e8a
Ryan Parrish <rino3717@gmail.com>
2020-03-26 01:30:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  sentence_array = string.split(" ")
+  sentence_array = sentence.split(" ")
   return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+  original_array.each do|sentence|
+    return_array << sentence.reverse
   end
   return_array.join(" ")
 end
